0196830ea0837133766d3c47e4675ace97af084a
reaon <yuj2029@gamil.com>
2024-02-14 20:19:36 +0900

    Login init

--- React-App/firebase-login/src/index.js
+++ React-App/firebase-login/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Auth from "./Auth";
+import Login from "./Components/Login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth />
+    <Login />
   </React.StrictMode>
 );
 
