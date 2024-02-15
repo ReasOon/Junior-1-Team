4b4052a11cf89a60b53c955de3d8fcdfacac9a4b
yeonizzajang <a18311831a@gmail.com>
2024-02-15 16:27:15 +0900

    Page_for_community

---
+++ React-App/firebase-login/src/Components/Page_for_community.js
@@ -0,0 +1,37 @@
+import './Page_for_community.css';
+import { useState } from 'react';
+
+function App() {
+  const [backcolor,setBackcolor]=useState("");
+  
+  const onClick =(color)=>{
+ setBackcolor(color)
+}
+ 
+  return (
+    <div>
+      <div>
+        Page for Community
+      </div>
+      
+      <div className='list'>
+
+      <div className='list-first'>
+      <button onClick={()=>onClick("yellow")}>Yellow</button>
+      <button onClick={()=>onClick("orange")}>Orange</button>
+      <button onClick={()=>onClick("green")}>Green</button>
+      <button onClick={()=>onClick("black")}>Black</button>
+      </div>
+
+      <div className='list-div'>
+          <div className='list-sec' style={{backgroundColor:backcolor}}></div>
+
+          <div className='profile-icon'></div>
+      </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Page_for_community;
